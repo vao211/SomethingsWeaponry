91faafe496772d2c99bb676fede976d47efb17ff
vao211 <Vaolu211@gmail.com>
2026-05-30 10:30:21 +0700

    chore

--- src/main/java/vao211/somethingsweaponry/config/WeaponConfig.java
+++ src/main/java/vao211/somethingsweaponry/config/WeaponConfig.java
@@ -8,8 +8,8 @@ public class WeaponConfig extends MidnightConfig {
     public static float oblivionSwordDamage = 9.0f;
     @Entry(category = "weapons")
     public static float oblivionSwordAttackSpeed = 1.3f;
-    @Entry
+    @Entry(category = "weapons")
     public static float oblivionDaggerDamage = 6.0f;
-    @Entry
+    @Entry(category = "weapons")
     public static float oblivionDaggerAttackSpeed = 2.0f;
 }
